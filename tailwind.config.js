29183e4e403576d93e4a4821b4e1fc009c382a90
socratesomiliadis <s.omiliadis@gmail.com>
2022-03-12 14:33:27 +0200

    Getting Started

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: "fade 1s ease-in-out",
+        w1: "w1Anim 1.5s infinite",
+        w2: "w2Anim 1.5s infinite",
+      },
+      keyframes: {
+        w1Anim: {
+          "0%": { opacity: "0" },
+          "1%": { opacity: "1" },
+          "49%": { opacity: "1" },
+          "50%": { opacity: "0" },
+        },
+        w2Anim: {
+          "50%": { opacity: "0" },
+          "51%": { opacity: "1" },
+          "99%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+        fade: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
